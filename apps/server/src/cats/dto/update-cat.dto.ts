3550ccc4c767c1485aff3072d9a8a8b0db659968
dileepbc01 <166901803+dileepbc01@users.noreply.github.com>
2025-01-07 13:05:19 +0530

    delete cat moule

--- apps/server/src/cats/dto/update-cat.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCatDto } from './create-cat.dto';
-import { IsString } from 'class-validator';
-export class UpdateCatDto extends PartialType(CreateCatDto) {
-  @IsString()
-  id: string;
-}
